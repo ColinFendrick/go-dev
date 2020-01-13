5f5a0ad02df7b1d8b184875aad25573f8521d331
Colin Fendrick <colin.fendrick@gmail.com>
2020-01-13 13:29:29 -0500

    :pencil: Some scope stuff

--- packages/main.go
+++ packages/main.go
@@ -4,11 +4,17 @@ import (
 	"fmt"
 )
 
+var a string
+var b = "string b"
+
 func main() {
 	// func Printf(format string, a ...interface{}) (n int, err error)
 	n, err := fmt.Println("Hello", 40, true)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Printf("Printed %v btyes", n)
+	fmt.Printf("Printed %v btyes \n", n)
+	a = "string a"
+	fmt.Println(a)
+	fmt.Println(b)
 }
