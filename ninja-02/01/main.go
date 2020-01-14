9ebfb2573654f4ab285be902ba27295fa5ac86cd
Colin Fendrick <colin.fendrick@gmail.com>
2020-01-14 09:11:53 -0500

    :sparkles: Hands on exercises #2

---
+++ ninja-02/01/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	n := 123
+	fmt.Printf("%d %b %x", n, n, n)
+}
---
+++ ninja-02/03/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+const (
+	a string = "henlo"
+	b        = 100
+)
+
+func main() {
+	const c = 200
+	fmt.Println(a, b, c)
+}
