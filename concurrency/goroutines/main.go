80b0df355d4df995d89d1cf398add43a3268ee89
Colin Fendrick <colin.fendrick@gmail.com>
2020-01-16 08:43:59 -0500

    :sparkles: Concurrency continued

---
+++ concurrency/goroutines/main.go
@@ -0,0 +1,24 @@
+/*
+They're called goroutines because the existing terms-
+threads, coroutines, processes, and so on—
+convey inaccurate connotations.
+A goroutine has a simple model:
+it is a function executing concurrently with other goroutines
+in the same address space.
+*/
+
+package main
+
+import "fmt"
+
+func doSomething(x int) int {
+	return x * 5
+}
+
+func main() {
+	ch := make(chan int)
+	go func() {
+		ch <- doSomething(5)
+	}()
+	fmt.Println(<-ch)
+}
---
+++ concurrency/race-condition/main.go
@@ -0,0 +1,32 @@
+// This is bad, unpredictable code
+
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	counter := 0
+
+	const gs = 100
+	var wg sync.WaitGroup
+	wg.Add(gs)
+
+	for i := 0; i < gs; i++ {
+		go func() {
+			v := counter
+			// time.Sleep(time.Second)
+			runtime.Gosched()
+			v++
+			counter = v
+			wg.Done()
+		}()
+		fmt.Println("Goroutines:", runtime.NumGoroutine())
+	}
+	wg.Wait()
+	fmt.Println("Goroutines:", runtime.NumGoroutine())
+	fmt.Println("count:", counter)
+}
